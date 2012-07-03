9b14059ebaa4e98bbd09fc5b7ae065083d975ae8
Deer-WarLord <deerwarlord@gmail.com>
2012-07-03 13:52:13 +0300

    re change path

--- Cats_lvl4/Settings.cs
+++ Cats_lvl4/Settings.cs
@@ -11,7 +11,7 @@ namespace Cats_lvl4
         public static string TestFilePath 
         {
             //get { return @"c:/Users/Tioma/Documents/Visual Studio 2010/GIT/DS_Cats/test_files/"; }
-            get { return @"C:/BuildAgent/work/9839fd882d9d77f0/Cats_lvl4/bin/Debug/"; }            
+            get { return @"C:/BuildAgent/work/9839fd882d9d77f0/test_files/"; }            
         }
     }
 }
